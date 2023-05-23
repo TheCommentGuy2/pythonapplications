7c3bf43bb0308a934d1e4781b3fcf3f86d922f14
TheCommentGuy2 <MuhammadUthman@proton.me>
2023-05-23 19:25:07 +1000

    minor change
    added a function where the screenshot minimizes if any place on the page is selected while it is enlarged, not only the image

--- ProccessTerminator/main.js
+++ ProccessTerminator/main.js
@@ -1,4 +1,11 @@
 function enlargeImage(image) {
     image.classList.toggle("enlarged");
   }
+
+  document.addEventListener("click", function(event) {
+    var image = document.getElementById("app-screenshot");
+    if (image.classList.contains("enlarged") && !image.contains(event.target)) {
+      image.classList.remove("enlarged");
+    }
+  });
   
